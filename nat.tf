ddd371dd8db41d1234db33d832b7183f2fe55f0f
LilanK <lilan.kularathna@gmail.com>
2020-11-17 12:33:55 +0530

    new changes

--- nat.tf
+++ nat.tf
@@ -12,12 +12,12 @@ resource "aws_route_table" "route_private" {
   vpc_id = aws_vpc.test_vpc.id
 
   route {
-    cidr_block = "10.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
 
   tags = {
-    Name = "private-route-table-demo"
+    Name = "private-route-table"
   }
 }
 
